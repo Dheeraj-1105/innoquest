92953fa25c59125ec16ae47ca2b55824c21fe05b
OLETI DHEERAJ KUMAR <23eg105v22@anurag.edu.in>
2025-10-17 18:54:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2416997879-5b608",
+  "appId": "1:589143272447:web:4d64e9d95d1c1b5bf795ca",
+  "apiKey": "AIzaSyDKtP9LisoI_LBLqPwY0dZ-wT4rqK-LALg",
+  "authDomain": "studio-2416997879-5b608.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "589143272447"
+};
